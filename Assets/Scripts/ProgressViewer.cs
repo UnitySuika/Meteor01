a0ca7d42331045f848e5d6254a9aea1458a951a1
SuikaBoy <hutadesu.2018@gmail.com>
2023-08-02 20:31:46 +0900

    Progress.csの生成

---
+++ Assets/Scripts/ProgressViewer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ProgressViewer : MonoBehaviour
+{
+    [SerializeField] MeteorSpawner meteorSpawner;
+
+    [SerializeField] TextMeshProUGUI text;
+
+    private void Update()
+    {
+        int count = meteorSpawner.GetLaterMeteorCount();
+        text.text = "‚ ‚Æ" + count;
+    }
+}
+
