ae189fb5a09042724c73e7cf30cde11b3d66ae62
Suika <69885751+UnitySuika@users.noreply.github.com>
2023-08-02 18:07:15 +0900

    Update ProgressView.cs

--- Assets/Scripts/ProgressView.cs
+++ Assets/Scripts/ProgressView.cs
@@ -12,6 +12,6 @@ public class ProgressView : MonoBehaviour
     private void Update()
     {
         int count = meteorSpawner.GetLaterMeteorCount();
-        text.text = $"Ç†Ç∆ {count}";
+        text.text = $"„ÅÇ„Å® {count}";
     }
 }
